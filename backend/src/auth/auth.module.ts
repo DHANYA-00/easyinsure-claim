5399c527af81fe9320fbd930cddf6fc02a162f12
DHANYA-00 <dhanyalakshmi.s.s.06@gmail.com>
2026-08-02 01:03:55 +0530

    feat: implement authentication logic

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from '../users/users.module';
 
@@ -6,7 +7,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
